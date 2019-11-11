81c4afedbc4f5c03ad097ee9f97ebb302724c473
irakli.tavberidze <irakli.tavberidze@webfg.com>
2019-11-11 15:33:54 +0100

    added

---
+++ components/guest.vue
@@ -0,0 +1,301 @@
+<template>
+  <v-container>
+    <v-layout class="main-hero" text-center wrap>
+      <v-flex mb-4>
+        <h1 class="headline">Bienvenido a Impact Hub Madrid</h1>
+        <p
+          class="subheading font-weight-regular"
+        >Indiqué en este formulario a quién ha venido a ver y le atenderemos en seguida, muchas gracias</p>
+      </v-flex>
+    </v-layout>
+    <v-layout class="main-container" text-center wrap>
+      <v-flex mb-4>
+        <form class="main-from">
+          <v-text-field
+            v-model="name"
+            :error-messages="nameErrors"
+            :counter="15"
+            label="Nombre"
+            required
+            @input="$v.name.$touch()"
+            @blur="$v.name.$touch()"
+          ></v-text-field>
+          <v-text-field
+            v-model="surname"
+            :error-messages="surnameErrors"
+            :counter="40"
+            label="Apellidos"
+            required
+            @input="$v.surname.$touch()"
+            @blur="$v.surname.$touch()"
+          ></v-text-field>
+          <v-text-field
+            v-model="email"
+            :error-messages="emailErrors"
+            label="E-mail"
+            required
+            @input="$v.email.$touch()"
+            @blur="$v.email.$touch()"
+          ></v-text-field>
+          <v-text-field
+            v-model="visitor"
+            :error-messages="visitorErrors"
+            :counter="40"
+            label="Vengo a ver a (nombre y apellidos)"
+            required
+            @input="$v.visitor.$touch()"
+            @blur="$v.visitor.$touch()"
+          ></v-text-field>
+          <v-checkbox
+            v-model="newsletter"
+            label="Sí, quiero suscribirme a la newsletter de Impact Hub"
+            @change="$v.newsletter.$touch()"
+            @blur="$v.newsletter.$touch()"
+          ></v-checkbox>
+          <v-checkbox
+            v-model="aceptoTodo"
+            :error-messages="aceptoTodoErrors"
+            :label="aviso"
+            required
+            @change="$v.aceptoTodo.$touch()"
+            @blur="$v.aceptoTodo.$touch()"
+          ></v-checkbox>
+
+          <div class="footer">
+            <v-btn class="send" @click="submit">Avisar de mi llegada</v-btn>
+            <v-btn @click="clear">Limpiar formulario</v-btn>
+          </div>
+        </form>
+      </v-flex>
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-card-title class="headline">Muchas gracias!</v-card-title>
+
+          <v-card-text>Se avisara enseguida a {{this.visitor}} de su llegada, gracias</v-card-text>
+
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn color="darken-1" text @click="clear">Cerrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import { validationMixin } from "vuelidate";
+import { required, maxLength, email } from "vuelidate/lib/validators";
+import Axios from "axios";
+
+export default {
+  mixins: [validationMixin],
+
+  validations: {
+    name: { required, maxLength: maxLength(15) },
+    surname: { required, maxLength: maxLength(40) },
+    visitor: { required, maxLength: maxLength(40) },
+    email: { required, email },
+    select: { required },
+    aceptoTodo: {
+      checked(val) {
+        return val;
+      }
+    },
+    newsletter: {
+      checked(val) {
+        return val;
+      }
+    }
+  },
+
+  data: () => ({
+    guest: [],
+    name: "",
+    surname: "",
+    visitor: "",
+    email: "",
+    aceptoTodo: false,
+    newsletter: true,
+    dialog: false,
+    aviso:
+      "Almacenaremos tus datos máximo 60 días," + "\n" + "¿estas de acuerdo?"
+  }),
+  computed: {
+    aceptoTodoErrors() {
+      const errors = [];
+      if (!this.$v.aceptoTodo.$dirty) return errors;
+      !this.$v.aceptoTodo.checked && errors.push("Este campo es obligatorio");
+      return errors;
+    },
+    nameErrors() {
+      const errors = [];
+      if (!this.$v.name.$dirty) return errors;
+      !this.$v.name.maxLength &&
+        errors.push("El nombre debe de contener máximo 15 caracteres ");
+      !this.$v.name.required && errors.push("Este campo es obligatorio");
+      return errors;
+    },
+    surnameErrors() {
+      const errors = [];
+      if (!this.$v.surname.$dirty) return errors;
+      !this.$v.surname.maxLength &&
+        errors.push("Los apellidos deben de contener máximo 40 caracteres ");
+      !this.$v.surname.required && errors.push("Este campo es obligatorio");
+      return errors;
+    },
+    visitorErrors() {
+      const errors = [];
+      if (!this.$v.visitor.$dirty) return errors;
+      !this.$v.visitor.maxLength &&
+        errors.push("El nombre debe de contener máximo 15 caracteres");
+      !this.$v.visitor.required && errors.push("Este campo es obligatorio");
+      return errors;
+    },
+    emailErrors() {
+      const errors = [];
+      if (!this.$v.email.$dirty) return errors;
+      !this.$v.email.email && errors.push("Tiene que ser un e-mail valido");
+      !this.$v.email.required && errors.push("Este campo es obligatorio");
+      return errors;
+    }
+  },
+
+  methods: {
+    submit() {
+
+      Axios.post("/user", {
+        request: 2,
+        username: this.surname,
+        name: this.name,
+        email: this.email
+      })
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.surname = "";
+      this.visitor = "";
+      this.email = "";
+      this.aceptoTodo = false;
+      this.dialog = false;
+    }
+  },
+  mounted() {
+    window.addEventListener("load", function() {
+      setTimeout(function() {
+        window.scrollTo(0, 1);
+      }, 0);
+    });
+  }
+};
+</script>
+<style scoped>
+@font-face {
+  font-family: GT Walsheim Pro;
+  src: local("GT Walsheim Pro Regular"), local("GTWalsheimProRegular"),
+    url("../assets/font/GTWalsheimProRegular.woff2") format("woff2"),
+    url("../assets/font/GTWalsheimProRegular.woff") format("woff"),
+    url("../assets/font/GTWalsheimProRegular.ttf") format("truetype");
+  font-weight: 400;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: GT Walsheim Pro;
+  src: local("GT Walsheim Pro Bold"), local("GTWalsheimProBold"),
+    url("../assets/font/GTWalsheimProBold.woff2") format("woff2"),
+    url("../assets/font/GTWalsheimProBold.woff") format("woff"),
+    url("../assets/font/GTWalsheimProBold.ttf") format("truetype");
+  font-weight: 700;
+  font-style: normal;
+}
+body {
+  background-color: #f3f3f3;
+}
+.container {
+  font-family: GT Walsheim Pro;
+  max-width: 100%;
+  padding: 0;
+}
+.v-application .headline {
+  font-family: GT Walsheim Pro !important;
+  font-weight: 700;
+  font-size: 2.3rem !important;
+  padding: 20px !important;
+  line-height: 1.1 !important;
+}
+p {
+  font-size: 18px;
+  font-weight: 400;
+  line-height: 1.45;
+}
+.main-container {
+  margin-top: 20px;
+  background-color: #fff;
+  width: 70%;
+  margin: auto;
+  margin-top: 20px;
+  border: 1px solid #eee;
+  border-bottom: 5px solid #ddd;
+  border-radius: 6px;
+}
+.main-hero {
+  background-color: #ee4f3f;
+  padding: 25px 0;
+}
+.main-hero h1,
+.main-hero p {
+  color: #fff;
+}
+.main-from {
+  padding: 25px 55px;
+}
+.send {
+  background-color: #005d66 !important;
+  color: #fff !important;
+  font-weight: bold;
+  margin: 0px;
+  width: 260px;
+}
+.v-btn {
+  font-size: 20px !important;
+  height: 85px !important;
+  width: 50%;
+  border-radius: 0px;
+}
+.flex.mb-4 {
+  max-width: 550px;
+  margin: auto;
+}
+.footer {
+  display: flex;
+  align-items: center;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  left: 0;
+}
+.v-card__actions .v-btn {
+  width: 100%;
+  background-color: #ddd !important;
+  color: #fff !important;
+  background-color: #005d66 !important;
+}
+.v-card__title.headline {
+  justify-content: center;
+  font-weight: bold !important;
+}
+.v-card__text {
+  justify-content: center;
+  text-align: center;
+  font-size: 20px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,44 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <h1><a href="/form">Ir al formulario</a></h1>
-      </div>
-    </v-flex>
-  </v-layout>
+  <v-app>
+    <div class="main-header" app>
+      <v-toolbar-title class="logo d-flex justify-center">
+        <img
+          data-no-lazy="1"
+          class="fl-logo-img"
+          itemscope
+          itemtype="https://schema.org/ImageObject"
+          src="https://madrid.impacthub.net/wp-content/uploads/2017/12/logo_ImpactHubMadrid-60px.svg"
+          data-retina="https://madrid.impacthub.net/wp-content/uploads/2017/09/logo-impact-hub-madrid-blanco.png"
+          alt="Impact Hub Madrid"
+        />
+      </v-toolbar-title>
+    </div>
+
+    <v-content>
+      <guest />
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import guest from "~/components/guest.vue";
 
 export default {
+  name: "App",
   components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+    guest
+  },
+  data: () => ({
+    //
+  })
+};
 </script>
+<style scoped>
+.logo {
+  width: 100%;
+}
+.main-header {
+  background-color: #812926;
+  padding: 10px;
+}
+</style>
